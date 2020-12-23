2cf313367eca031415f1ed368f8a53938595d464
williamfzc <178894043@qq.com>
2020-12-23 10:15:31 +0000

    test: instrumentation cases

--- demo/app/src/androidTest/java/com/github/williamfzc/webvpm_demo/ExampleInstrumentedTest.kt
+++ demo/app/src/androidTest/java/com/github/williamfzc/webvpm_demo/ExampleInstrumentedTest.kt
@@ -1,12 +1,21 @@
 package com.github.williamfzc.webvpm_demo
 
-import androidx.test.InstrumentationRegistry
-import androidx.test.runner.AndroidJUnit4
-
+import android.util.Log
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Test
 import org.junit.runner.RunWith
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.test.core.app.ApplicationProvider
+import com.github.williamfzc.wvpm.WvpmAPI
+import com.github.williamfzc.wvpm.WvpmJsFlag
+import com.github.williamfzc.wvpm.WvpmJsFlagBase
+import com.github.williamfzc.wvpm.WvpmResponse
+import com.github.williamfzc.wvpm.js.WvpmJsContent
+import com.github.williamfzc.wvpm.js.WvpmJsManager
 
-import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +24,61 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    lateinit var targetWebview: WebView
+    val TAG = "androidTest"
+    val URL = "http://baidu.com"
+
+    // custom hook if you need
+    enum class MyJsFlag : WvpmJsFlagBase { FLAG_NEW }
+
+    val MyJsContent = object : WvpmJsContent(path = "perf.js") {}
+
+    @Before
+    fun prepare() {
+        InstrumentationRegistry.getInstrumentation().runOnMainSync {
+            targetWebview = WebView(ApplicationProvider.getApplicationContext())
+        }
+    }
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.github.williamfzc.webvpm", appContext.packageName)
+    fun testInject() {
+        InstrumentationRegistry.getInstrumentation().runOnMainSync {
+            // if we already have a client
+            targetWebview.webViewClient = object : WebViewClient() {
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    Log.d(TAG, "origin page finished!")
+                }
+            }
+
+            // custom hook
+            WvpmJsManager.addJs(MainActivity.MyJsFlag.FLAG_NEW, MyJsContent)
+            WvpmAPI.injectOnPageFinished(targetWebview, MainActivity.MyJsFlag.FLAG_NEW) {
+                Log.d(TAG, "custom js: ${it.data}")
+            }
+
+            WvpmAPI.injectOnPageFinished(
+                targetWebview,
+                WvpmJsFlag.FLAG_JS_PERF_TIMING,
+                fun(resp: WvpmResponse) {
+                    Log.d(TAG, "get js return after page finished in activity: ${resp.data}")
+                })
+            WvpmAPI.injectOnPageFinished(
+                targetWebview,
+                WvpmJsFlag.FLAG_JS_PERF_NAVIGATION,
+                fun(resp: WvpmResponse) {
+                    Log.d(TAG, "again: get js return after page finished in activity: ${resp.data}")
+                })
+            WvpmAPI.injectOnPageStarted(
+                targetWebview,
+                WvpmJsFlag.FLAG_JS_PERF_TIMING,
+                fun(resp: WvpmResponse) {
+                    Log.d(TAG, "get js return before page started in activity: ${resp.data}")
+                })
+            targetWebview.loadUrl(URL)
+
+            // wait for callback finished
+            Thread.sleep(5000)
+        }
     }
 }
